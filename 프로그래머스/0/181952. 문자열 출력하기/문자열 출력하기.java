6072a1cc6566bacfccedb3418b575d8b191b2ed3
ks07235 <amg07235@gmail.com>
2026-02-04 14:50:00 +0900

    Update 문자열 출력하기.java

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -5,5 +5,14 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         System.out.print(a);
+        /* 
+        오랜만에 해서 print 좀 걸림.
+        이클립스에서도 Sysout으로 하다보니까 잊어버렸었음
+        지금부터는 코드의 기초부터 다시 다질 예정임
+        최대한 출력과 비슷하게 1번
+        실무를 가장했을때 출력한다면 1번
+        최대한 리소스를 덜먹는 방법 1번
+        리팩토링 1번 이런식으로 최소 4번은 생각해볼 예정
+        */
     }
 }
